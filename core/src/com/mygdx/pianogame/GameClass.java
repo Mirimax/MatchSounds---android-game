4794df0f3dadaec7b59288b5cbea2a0de86f169f
Kipowiec <56412174+Kipowiec@users.noreply.github.com>
2020-12-03 18:32:00 +0100

    Add initialization of fonts and skins and change the way that displays screen changes.
    Now instances of each screen are created once, not each time the screen was changing.

--- core/src/com/mygdx/pianogame/GameClass.java
+++ core/src/com/mygdx/pianogame/GameClass.java
@@ -1,29 +1,48 @@
 package com.mygdx.pianogame;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mygdx.pianogame.screens.LoadingScreen;
+import com.mygdx.pianogame.screens.MatchSounds;
 import com.mygdx.pianogame.screens.Menu;
+
 
 
 public class GameClass extends Game {
 	public OrthographicCamera camera;
 	public AssetManager assetManager;
+
+	public BitmapFont font;
+	public BitmapFont fontBig;
+	public SpriteBatch batch;
+	public Skin skin;
+	public LoadingScreen loadingScreen;
+	public Menu menu;
+	public MatchSounds matchSounds;
+
+
 	@Override
 	public void create () {
 		camera = new OrthographicCamera();
-		camera.setToOrtho(true);
+		camera.setToOrtho(false);
 		assetManager = new AssetManager();
-		this.setScreen(new LoadingScreen(this));
-		//storing sounds of single notes
+		batch = new SpriteBatch();
+
+		initFonts();
+		initSkins();
+
+		loadingScreen = new LoadingScreen(this);
+		menu = new Menu(this);
+		matchSounds = new MatchSounds(this);
+		this.setScreen(loadingScreen);
 	}
 
 	@Override
@@ -33,7 +52,32 @@ public class GameClass extends Game {
 	
 	@Override
 	public void dispose () {
-		this.getScreen().dispose();
+		batch.dispose();
+		font.dispose();
+		fontBig.dispose();
+		skin.dispose();
+		assetManager.dispose();
+		loadingScreen.dispose();
+		menu.dispose();
+		matchSounds.dispose();
+	}
+
+	private void initFonts(){
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Arcon.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.size = 60;
+		parameter.color = Color.BLACK;
+		font = generator.generateFont(parameter);
+		FreeTypeFontGenerator.FreeTypeFontParameter parameterBig = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameterBig.size = 120;
+		parameterBig.color = Color.BLACK;
+		fontBig = generator.generateFont(parameterBig);
+	}
+	private void initSkins(){
+		skin = new Skin();
+		skin.addRegions(new TextureAtlas(Gdx.files.internal("ui/uiskin.atlas")));
+		skin.add("default-font",this.font);
+		skin.load(Gdx.files.internal("ui/uiskin.json"));
 	}
 
 }
