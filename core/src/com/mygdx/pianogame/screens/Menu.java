1439c6ca79c1daee869199219cf55714246bd795
Kipowiec <56412174+Kipowiec@users.noreply.github.com>
2020-12-04 12:02:28 +0100

    Add 2 buttons that are game modes and one exit button. Add the title of the game at top-center.

--- core/src/com/mygdx/pianogame/screens/Menu.java
+++ core/src/com/mygdx/pianogame/screens/Menu.java
@@ -1,72 +1,114 @@
 package com.mygdx.pianogame.screens;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.pianogame.GameClass;
-import com.mygdx.pianogame.SimpleButton;
 
 
 public class Menu implements Screen {
     private final GameClass app;
     private Stage stage;
-    private Image matchSoundsImg;
+
+    private TextButton.TextButtonStyle textButtonStyle;
+
+    private GlyphLayout mathSoundText;
 
     public Menu(final GameClass app){
         this.app = app;
         stage = new Stage(new FitViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight()));
-        Gdx.input.setInputProcessor(stage);
-
-
-        //Adding game mode nr.1 MatchSound
-        Texture matchSoundsTex = app.assetManager.get("img/buttons/option1.png",Texture.class);
-        matchSoundsImg = new Image(matchSoundsTex);
-        matchSoundsImg.addListener(new InputListener(){
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                ((GameClass)Gdx.app.getApplicationListener()).dispose();
-                ((GameClass)Gdx.app.getApplicationListener()).setScreen(new MatchSounds(app));
-                return true;
-            }
-        });
-        stage.addActor(matchSoundsImg);
-
-
     }
 
     @Override
     public void show() {
-        matchSoundsImg.setPosition(Gdx.graphics.getWidth()/2 - matchSoundsImg.getWidth()/2,Gdx.graphics.getHeight()*4/5 - matchSoundsImg.getHeight()/2);
+        stage.clear();
+        Gdx.input.setInputProcessor(stage);
+
+        //Text of the title of game
+        mathSoundText = new GlyphLayout(app.fontBig,"Match sounds");
+
+        initButtons();
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1f,1f,1f,1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //Displaying the title of game
+        app.batch.begin();
+        app.fontBig.draw(app.batch,mathSoundText,Gdx.graphics.getWidth()/2f - mathSoundText.width/2,Gdx.graphics.getHeight()- 100 - mathSoundText.height);
+        app.batch.end();
+
         update(delta);
-		stage.draw();
+        stage.draw();
     }
 
     private void update(float delta) {
-        stage.act(Gdx.graphics.getDeltaTime());
+        stage.act(delta);
     }
 
     @Override
     public void resize(int width, int height) {
 
+    }
+
+    private void initButtons(){
+        //Option nr.1 - Survival mode
+        TextButton survivalMode = new TextButton("Survival", app.skin);
+        survivalMode.setPosition(stage.getWidth()/2 - 300,stage.getHeight()/2);
+        survivalMode.setSize(600f,200f);
+//        survivalMode.setColor(0.302f,0.302f,0.302f,1f);
+        survivalMode.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                ((GameClass)Gdx.app.getApplicationListener()).setScreen(app.matchSounds);
+            }
+        });
+        stage.addActor(survivalMode);
+
+        //Option nr.2 - Custom mode
+        TextButton customMode = new TextButton("Custom", app.skin, "default");
+        customMode.setPosition(stage.getWidth()/2 - 300,stage.getHeight()/2-225);
+        customMode.setSize(600,200);
+        customMode.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                ((GameClass)Gdx.app.getApplicationListener()).setScreen(app.matchSounds);
+            }
+        });
+        stage.addActor(customMode);
+
+        //Option nr.3 - Exit from game
+        TextButton exit = new TextButton("Exit", app.skin, "default");
+        exit.setPosition(stage.getWidth()/2 - 300,stage.getHeight()/2 - 450);
+        exit.setSize(600,200);
+        exit.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.exit();
+            }
+        });
+        stage.addActor(exit);
     }
 
     @Override
